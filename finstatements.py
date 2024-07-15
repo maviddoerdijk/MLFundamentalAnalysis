250ddd328986264fa7298e476b327f82e40a01ab
david <davidmoerdijk@gmail.com>
2024-07-15 09:10:49 +0200

    [add]

---
+++ finstatements.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import numpy as np
+from pathlib import Path
+import requests
+from zipfile import ZipFile
+from io import BytesIO
+
+data_path = Path('data')
+
+SEC_URL = 'https://www.sec.gov/files/dera/data/financial-statement-and-notes-data-sets/'
+first_year, this_year, this_quarter = 2014, 2018, 3
+past_years = range(2014, this_year)
+filing_periods = [(y, q) for y in past_years for q in range(1, 5)]
+filing_periods.extend([(this_year, q) for q in range(1, this_quarter + 
+                                                    1)])
+for i, (yr, qtr) in enumerate(filing_periods, 1):
+    filing = f'{yr}q{qtr}_notes.zip'
+    path = data_path / f'{yr}_{qtr}' / 'source'
+    response = requests.get(SEC_URL + filing).content
+    with ZipFile(BytesIO(response)) as zip_file:
+        for file in zip_file.namelist():
+            local_file = path / file
+            with local_file.open('wb') as output:
+                for line in zip_file.open(file).readlines():
+                    output.write(line)
+                    
+# The data is fairly large, and to enable faster access than the original text files permit, 
+# it is better to convert the text files into a binary, Parquet columnar format 
+
+
+for f in data_path.glob('**/*.tsv'):
+    file_name = f.stem  + '.parquet'
+    path = Path(f.parents[1]) / 'parquet'
+    df = pd.read_csv(f, sep='\t', encoding='latin1', low_memory=False)
+    df.to_parquet(path / file_name)
+
+
+apple = sub[sub.name == 'APPLE INC'].T.dropna().squeeze()
+key_cols = ['name', 'adsh', 'cik', 'name', 'sic', 'countryba',  
+            'stprba', 'cityba', 'zipba', 'bas1', 'form', 'period', 
+            'fy', 'fp', 'filed']
+apple.loc[key_cols]
+
+
+key_cols = ['name', 'adsh', 'cik', 'name', 'sic', 'countryba',  
+            'stprba', 'cityba', 'zipba', 'bas1', 'form', 'period', 
+            'fy', 'fp', 'filed']
+apple.loc[key_cols]
+
+aapl_subs = pd.DataFrame()
+for sub in data_path.glob('**/sub.parquet'):
+    sub = pd.read_parquet(sub)
+    aapl_sub = sub[(sub.cik.astype(int) == apple.cik) & 
+                   (sub.form.isin(['10-Q', '10-K']))]
+    aapl_subs = pd.concat([aapl_subs, aapl_sub])
+
+aapl_nums = pd.DataFrame()
+for num in data_path.glob('**/num.parquet'):
+    num = pd.read_parquet(num).drop('dimh', axis=1)
+    aapl_num = num[num.adsh.isin(aapl_subs.adsh)]
+    aapl_nums = pd.concat([aapl_nums, aapl_num])
+aapl_nums.ddate = pd.to_datetime(aapl_nums.ddate, format='%Y%m%d')
+
+import pandas_datareader.data as web
+symbol = 'AAPL.US'
+aapl_stock = web.DataReader(symbol, 'quandl', start=eps.index.min())
+aapl_stock = aapl_stock.resample('D').last() # ensure dates align with 
+
+pe = aapl_stock.AdjClose.to_frame('price').join(eps.to_frame('eps'))
+pe = pe.fillna(method='ffill').dropna()
+pe['P/E Ratio'] = pe.price.div(pe.eps)
+axes = pe.plot(subplots=True, figsize=(16,8), legend=False, lw=2)                                             
+                                            
+                                               
